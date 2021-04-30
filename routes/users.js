a05f62f76f93d014c594bd81ae0a2e37a079a6da
mandyho0703 <mandyho0703@gmail.com>
2021-04-30 14:58:56 +0000

    creating views

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+// Login Page
+router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
+
+// Register Page
+router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
